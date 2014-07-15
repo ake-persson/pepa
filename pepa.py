c69468423104262ae6d981dc814e5ba0b126b57d
Michael Persson <michael.persson@imc.nl>
2014-07-15 13:30:09 +0200

    Removed inputs made them part of templates

--- pepa.py
+++ pepa.py
@@ -1,7 +1,7 @@
 __author__ = 'Michael Persson <michael.ake.persson@gmail.com>'
 __copyright__ = 'Copyright (c) 2013 Michael Persson'
 __license__ = 'GPLv3'
-__version__ = '0.5.6'
+__version__ = '0.6.2'
 
 # Import python libs
 from salt.exceptions import SaltInvocationError
@@ -21,7 +21,7 @@ __opts__ = {
     'pepa_delimiter': '..'
 }
 
-# Import third party libs
+# Import libraries
 try:
     import yaml
     HAS_YAML = True
@@ -29,7 +29,7 @@ except ImportError:
     HAS_YAML = False
 
 try:
-    from os.path import isfile, join as makepath
+    from os.path import isfile, join
     HAS_OS_PATH = True
 except ImportError:
     HAS_OS_PATH = False
@@ -48,26 +48,24 @@ except ImportError:
 
 def __virtual__():
     if not HAS_YAML:
-        log.error("Pepa ext_pillar failed to load YAML library")
+        log.error('Failed to load "yaml" library')
         return False
 
     if not HAS_OS_PATH:
-        log.error("Pepa ext_pillar failed to load OS Path library")
+        log.error('Failed to load "os.path" library')
         return False
 
     if not HAS_RE:
-        log.error("Pepa ext_pillar failed to load RE library")
+        log.error('Failed to load "re" library')
         return False
 
     if not HAS_JINJA2:
-        log.error("Pepa ext_pillar failed to load Jinja2 library")
+        log.error('Failed to load "jinja2" library')
         return False
 
-    log.debug("Loading Pepa ext_pillar")
     return(__virtualname__)
 
-# Convert key/value to nested dictionary
-# Plundered with impunity from salt-pillar-dynamo (https://github.com/jasondenning/salt-pillar-dynamo)
+# Convert key/value to tree
 def key_value_to_tree(data):
     tree = {}
     for flatkey, value in data.items():
@@ -80,52 +78,50 @@ def key_value_to_tree(data):
                 t = t.setdefault(key, {})
     return(tree)
 
-def ext_pillar(minion_id, pillar, resource, sequence, baseonly):
+def ext_pillar(minion_id, pillar, resource, sequence):
     roots = __opts__['pepa_roots']
 
-    # Validate roots
-
-    # Validate sequence
-    if not sequence:
-        raise SaltInvocationError('ext_pillar.pepa: sequence is not defined')
-    if type(sequence) is not list:
-        raise SaltInvocationError('ext_pillar.pepa: sequence needs to be a list')
-
+    # Default input
     input = {}
     input['default'] = 'default'
-
-    # Load input
-    fn = makepath(roots['base'], resource, 'inputs', minion_id + '.yaml')
-    if isfile(fn):
-        log.debug("Pepa ext_pillar load host input: %s" % fn)
-        input.update(yaml.load(open(fn).read()))
-        input['pepa_input'] = fn
+    input['hostname'] = minion_id
+    if 'environment' in __grains__:
+        input['environment'] = __grains__['environment']
     else:
-        log.error("Pepa ext_pillar host input doesn't exist: %s" % fn)
-        input['pepa_input'] = 'None'
+        input['environment'] = 'base'
 
     # Load templates
     output = input
     output['pepa_templates'] = []
-    for category in sequence:
-        if category not in input:
+
+    for name, info in [s.items()[0] for s in sequence]:
+
+        if name not in input:
             continue
 
-        entries = []
-        if type(input[category]) is list:
-            entries = input[category]
+        alias = None
+        if type(info) is dict and 'name' in info:
+            alias = info['name']
         else:
-            entries = [ input[category] ]
+            alias = name
+
+        templdir = None
+        if info and 'base_only' in info and info['base_only']:
+            templdir = join(roots['base'], resource, alias)
+        else:
+            templdir = join(roots[input['environment']], resource, alias)
+
+        entries = []
+        if type(input[name]) is list:
+            entries = input[name]
+        else:
+            entries = [ input[name] ]
 
         for entry in entries:
             results = None
-            fn = None
-            if category in baseonly:
-                fn = makepath(roots['base'], resource, 'templates', category, re.sub('\W', '_', entry.lower()) + '.yaml')
-            else:
-                fn = makepath(roots[input['environment']], resource, 'templates', category, re.sub('\W', '_', entry.lower()) + '.yaml')
+            fn = join(templdir, re.sub('\W', '_', entry.lower()) + '.yaml')
             if isfile(fn):
-                log.debug("Pepa ext_pillar load template: %s" % fn)
+                log.debug("Loading template: %s" % fn)
                 template = jinja2.Template(open(fn).read())
                 output['pepa_templates'].append(fn)
                 data = key_value_to_tree(output)
@@ -133,12 +129,12 @@ def ext_pillar(minion_id, pillar, resource, sequence, baseonly):
                 data['pillar'] = pillar.copy()
                 results = yaml.load(template.render(data))
             else:
-                log.debug("Pepa ext_pillar template doesn't exist: %s" % fn)
+                log.debug("Template doesn't exist: %s" % fn)
                 continue
 
             if results != None:
                 for key in results:
-                    log.debug("Pepa ext_pillar substituting key: %s value: %s" % (key, results[key]))
+                    log.debug("Substituting key: %s value: %s" % (key, results[key]))
                     output[key] = results[key]
 
     tree = key_value_to_tree(output)
