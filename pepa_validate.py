7536d49167498718d1d8419daecb9eb143fe1512
Michael Persson <michael.persson@imc.nl>
2014-09-30 16:49:27 +0200

    Added cerberus validation to pepa_validate

--- pepa_validate.py
+++ pepa_validate.py
@@ -18,6 +18,10 @@ import jinja2
 import re
 from os.path import isfile, isdir, join, dirname, basename
 import argparse
+import cerberus
+import pygments
+import pygments.lexers
+import pygments.formatters
 
 # Options
 __opts__ = {
@@ -49,6 +53,12 @@ def validate_templates():
     Parse Pepa templates
     '''
     success = True
+    resdir = join(roots['base'], resource)
+    schema = {}
+    if isfile(resdir + '/schema.yaml'):
+        log.debug('Load schema {0}'.format(resdir + '/schema.yaml'))
+        schema = yaml.load(open(resdir + '/schema.yaml').read())
+
     for categ, info in [s.items()[0] for s in sequence]:
         templdir = join(roots['base'], resource, categ)
         if isinstance(info, dict) and 'name' in info:
@@ -66,24 +76,38 @@ def validate_templates():
                 log.debug('Load template {0}'.format(fn))
 
                 template = jinja2.Template(open(fn).read())
-                result = None
+                res_jinja = None
+                res_yaml = None
                 try:
-                    result = template.render(defaults)
+                    res_jinja = template.render(defaults)
                 except Exception, e:
                     success = False
                     log.error('Failed to parse JINJA template {0}\n{1}'.format(fn, e))
                     continue
                 try:
-                    yaml.load(result)
+                    res_yaml = yaml.load(res_jinja)
                 except Exception, e:
                     success = False
                     log.error('Failed to parse YAML in template {0}\n{1}'.format(fn, e))
+
+                if args.show:
+                    print pygments.highlight(yaml.safe_dump(res_yaml), pygments.lexers.YamlLexer(), pygments.formatters.TerminalFormatter())
+
+                val = cerberus.Validator()
+                try:
+                    status = val.validate(res_yaml, schema)
+                    if not status:
+                        for ekey, error in val.errors.items():
+                            log.warning('Validation failed for key {0}: {1}'.format(ekey, error))
+                except Exception, e:
+                    log.warn('Failed to validate output for template {0}\n{1}'.format(fn, e))
 
     return success
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', '--config', default='/etc/salt/master', help='Configuration file')
 parser.add_argument('-d', '--debug', action='store_true', help='Print debug info')
+parser.add_argument('-s', '--show', action='store_true', help='Show result of template')
 parser.add_argument('-n', '--no-color', action='store_true', help='No color output')
 parser.add_argument('-t', '--teamcity', action='store_true', help='Output validation in TeamCity format')
 args = parser.parse_args()
